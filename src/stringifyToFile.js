2fe4bdcf34a0b1bbaa3c3f0ab80a084e69810583
loldonut <loldonutph@gmail.com>
2022-06-17 15:07:23 +0800

    Add description of `stringifyToFile`

--- src/stringifyToFile.js
+++ src/stringifyToFile.js
@@ -3,6 +3,7 @@ const { writeFileSync } = require('fs');
 const stringify = require('./stringify');
 
 /**
+ * Stringify the Object then write it to the output file
  *
  * @param {string} outputFilename 
  * @param {Object} data 
